28c491d15ea921c95dce28a6bc9479686c854f1f
luidimso <luidi_mso@outlook.com>
2023-12-05 13:43:33 -0300

    Implementing Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: "1",
+        title: "The first",
+        image: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/portals_3/2x1_SuperMarioHub_image1600w.jpg",
+        address: "Rua do Teste",
+        description: "Test 1"
+    },
+    {
+        id: "2",
+        title: "The second",
+        image: "https://2.bp.blogspot.com/-5l05v-QNv-w/UVd-HrwdVdI/AAAAAAAAByI/u4KBuk7zzpE/s640/O+Ano+do+Luigi.jpg",
+        address: "Rua do Teste",
+        description: "Test 2"
+    },
+    {
+        id: "2",
+        title: "The third",
+        image: "https://jogoveio.com.br/wp-content/uploads/2022/10/thumb-peach-marioparty.jpg",
+        address: "Rua do Teste",
+        description: "Test 3"
+    }
+];
+
 export default function HomePage() {
-    return <p>Home Page</p>;
+    return <MeetupList meetups={DUMMY_MEETUPS}></MeetupList>;
 }
