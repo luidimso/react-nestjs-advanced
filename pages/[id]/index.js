540e6e53d9fa489610e21832809b5b02476bc3e0
luidimso <luidi_mso@outlook.com>
2023-12-05 15:46:58 -0300

    Using getStaticProps to get path params from context on page

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,3 +1,17 @@
 export default function MeetupPage() {
     return <p>Meetup Page</p>;
 }
+
+export async function getStaticProps(context) {
+    const id = context.params.id;
+
+    console.log(id);
+
+    return {
+        props: {
+            meetupData: {
+                id: id
+            }
+        }
+    };
+}
