eacbc38b0c209050d03aa3cc169987fa9c1b91c3
luidimso <luidi_mso@outlook.com>
2023-12-06 11:56:04 -0300

    Implementing meetup details page

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,21 +1,32 @@
-export default function MeetupPage() {
-    return <p>Meetup Page</p>;
+import { MongoClient, ObjectId } from "mongodb";
+import { Fragment } from "react";
+
+export default function MeetupPage(props) {
+    return <Fragment>
+        <h3>{props.title}</h3>
+        <img height={100} hsrc={props.image}></img>
+        <p>{props.address}</p>
+        <p>{props.description}</p>
+    </Fragment>;
 }
 
 export async function getStaticPaths() {
+    const client = await MongoClient.connect("mongodb+srv://luidimso:El_Loco-13@cluster0.okownu3.mongodb.net/?retryWrites=true&w=majority");
+
+    const db = client.db();
+
+    const collection = db.collection("meetups");
+
+    const meettups = await collection.find({}, {_id: 1}).toArray();
+
+    client.close();
+
     return {
-        paths: [
-            {
-                params: {
-                    id: "1"
-                }
-            },
-            {
-                params: {
-                    id: "2"
-                }
+        paths: meettups.map((meetup => ({
+            params: {
+                id: meetup._id.toString()
             }
-        ],
+        }))),
         fallback: false
     };
 }
@@ -23,13 +34,24 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
     const id = context.params.id;
 
-    console.log(id);
+    const client = await MongoClient.connect("mongodb+srv://luidimso:El_Loco-13@cluster0.okownu3.mongodb.net/?retryWrites=true&w=majority");
+
+    const db = client.db();
+
+    const collection = db.collection("meetups");
+
+    const selectedMeetup = await collection.findOne({_id: new ObjectId(id)});
+
+    console.log(selectedMeetup)
+
+    client.close();
 
     return {
         props: {
-            meetupData: {
-                id: id
-            }
+            _id: selectedMeetup._id.toString(),
+            title: selectedMeetup.title,
+            address: selectedMeetup.address,
+            description: selectedMeetup.description
         }
     };
 }
