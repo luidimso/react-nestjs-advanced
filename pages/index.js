c8b003a57a094e181b319d630f16a8dfaf506f18
luidimso <luidi_mso@outlook.com>
2023-12-06 05:10:08 -0300

    Implementing the request on getStaticProps on HomePage to get from MongoDB the meetups

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-    {
-        id: "1",
-        title: "The first",
-        image: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/portals_3/2x1_SuperMarioHub_image1600w.jpg",
-        address: "Rua do Teste",
-        description: "Test 1"
-    },
-    {
-        id: "2",
-        title: "The second",
-        image: "https://2.bp.blogspot.com/-5l05v-QNv-w/UVd-HrwdVdI/AAAAAAAAByI/u4KBuk7zzpE/s640/O+Ano+do+Luigi.jpg",
-        address: "Rua do Teste",
-        description: "Test 2"
-    },
-    {
-        id: "3",
-        title: "The third",
-        image: "https://jogoveio.com.br/wp-content/uploads/2022/10/thumb-peach-marioparty.jpg",
-        address: "Rua do Teste",
-        description: "Test 3"
-    }
-];
+export default function HomePage(props) {
+    return <MeetupList meetups={props.meetups}></MeetupList>;
+}
 
-export default function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS}></MeetupList>;
+export async function getStaticProps() {
+    const client = await MongoClient.connect("mongodb+srv://luidimso:El_Loco-13@cluster0.okownu3.mongodb.net/?retryWrites=true&w=majority");
+
+    const db = client.db();
+
+    const collection = db.collection("meetups");
+
+    const meettups = await collection.find().toArray();
+
+    client.close();
+
+    return {
+        props: {
+            meetups: meettups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                description: meetup.description,
+                id: meetup._id.toString()
+            }))
+        },
+        revalidate: 1
+    };
 }
