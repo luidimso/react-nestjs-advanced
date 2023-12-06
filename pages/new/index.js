a5f9f0f49df21e3b1543999bf7830c3a4d06cfea
luidimso <luidi_mso@outlook.com>
2023-12-06 04:47:11 -0300

    Implementing the request to API route to send POST request to store data in MOngoDB database

--- pages/new/index.js
+++ pages/new/index.js
@@ -1,8 +1,23 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewPage() {
-    function onAddMeetup(meetupData) {
-        console.log(meetupData);
+    const router = useRouter();
+
+    async function onAddMeetup(meetupData) {
+        const request = await fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(meetupData),
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+
+        const data = await request.json();
+
+        console.log(data);
+
+        router.replace("/");
     }
 
     return <NewMeetupForm onAddMeetup={onAddMeetup}></NewMeetupForm>;
