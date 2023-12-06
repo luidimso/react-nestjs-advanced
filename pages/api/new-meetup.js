a286938ddd8f8a0978a0c886f7dc1d165003a31a
luidimso <luidi_mso@outlook.com>
2023-12-06 04:24:42 -0300

    Implementing the connection with MongoDB through the API

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
     if(req.method === "POST") {
         const data = req.body;
 
-        const {title, image, address, description} = date;
+        const client = await MongoClient.connect("mongodb+srv://luidimso:El_Loco-13@cluster0.okownu3.mongodb.net/?retryWrites=true&w=majority");
+
+        const db = client.db();
+
+        const collection = db.collection("meetups");
+
+        const result = await collection.insertOne(data)
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({
+            message: "Meetup inserted"
+        });
     }
 }
 
